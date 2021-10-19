3fc9d59c1bd9cf8e33a7ede898451b2de0eee8cd
d4nielkiss <d4niel.kiss@gmail.com>
2021-10-19 11:33:05 +0200

    environment variables cofigured

---
+++ frontend/src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  protocol: process.env.REACT_APP_BACKEND_PROTOCOL,
+  host: process.env.REACT_APP_BACKEND_HOST,
+  port: process.env.REACT_APP_BACKEND_PORT,
+  route: process.env.REACT_APP_BACKEND_ROUTE,
+};
+
+export default config;
