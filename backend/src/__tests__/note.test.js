3c62b9d268a5c8bf3eb474705ee0cd98df7490e8
d4nielkiss <d4niel.kiss@gmail.com>
2021-11-26 14:44:25 +0100

    note tested on the length of description

--- backend/src/__tests__/note.test.js
+++ backend/src/__tests__/note.test.js
@@ -102,4 +102,19 @@ describe('POST api/note', () => {
       expect(response.body.error).toBe('Missing author ID');
     });
   });
+
+  describe('given data but too long description', () => {
+    test('should respond with status code 400 and error message', async () => {
+      const noteData = {
+        title: 'The title',
+        description: 'Aenean lacinia vehicula ante, interdum sagittis nulla porttitor ut. Vivamus ut nisi ullamcorper, porta mi in, maximus nulla. Nam luctus aliquam est ac hendrerit. Nulla eleifend mauris sed nibh convallis efficitur. Aenean lacinia vehicula ante, interdum sagittis nulla porttitor ut. Vivamus ut nisi ullamcorper, porta mi in, maximus nulla. Nam luctus aliquam est ac hendrerit. Nulla eleifend mauris sed nibh convallis efficitur. Aenean lacinia vehicula ante, interdum sagittis nulla porttitor ut. Vivamus ut nisi ullamcorper, porta mi in, maximus nulla. Nam luctus aliquam est ac hendrerit. Nulla eleifend mauris sed nibh convallis efficitur. Aenean lacinia vehicula ante, interdum sagittis nulla porttitor ut. Vivamus ut nisi ullamcorper, porta mi in, maximus nulla. Nam luctus aliquam est ac hendrerit. Nulla eleifend mauris sed nibh convallis efficitur. Aenean lacinia vehicula ante, interdum sagittis nulla porttitor ut. Vivamus ut nisi ullamcorper, porta mi in, maximus nulla. Nam er, porta mi ina',
+        date: Date.now(),
+        isPinned: false,
+        authorId: '6107a327b1c3c1003b3b126e',
+      };
+      const response = await request(app).post('/api/note').send(noteData);
+      expect(response.statusCode).toBe(400);
+      expect(response.body.error).toBe('Description cannot be longer than 1000 characters');
+    });
+  });
 })
